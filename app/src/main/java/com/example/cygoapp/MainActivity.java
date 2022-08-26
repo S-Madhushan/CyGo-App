8bff934020c9e7e119e19b2caa1a42ed9a5e8240
S-Madhushan <suranjimadhushan@gmail.com>
2022-08-26 23:33:05 +0530

    Uploading

---
+++ app/src/main/java/com/example/cygoapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.cygoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+
+    Animation topAnim;
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+
+        logo = findViewById(R.id.logoImg);
+
+        logo.setAnimation(topAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View,String>(logo,"logoTransition");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
