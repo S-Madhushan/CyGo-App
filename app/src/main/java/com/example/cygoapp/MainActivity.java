bbf44ae9290d85cd31bd6d21531d6070174c361c
NDbiz <nadeeshadilshan6@gmail.com>
2022-09-06 23:02:42 +0530

    done splash and get started

--- app/src/main/java/com/example/cygoapp/MainActivity.java
+++ app/src/main/java/com/example/cygoapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.cygoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Pair;
@@ -14,11 +16,13 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String prevStarted = "yes";
     private static int SPLASH_SCREEN = 3000;
 
     Animation topAnim;
-    ImageView logo;
+    ImageView cygo;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +30,56 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        if(!sharedPreferences.getBoolean(prevStarted,false)){
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(prevStarted,Boolean.TRUE);
+            editor.apply();
+
+            topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+
+            cygo = findViewById(R.id.logo);
+
+            cygo.setAnimation(topAnim);
+
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this,activity_getStart.class);
+                    Pair[] pairs = new Pair[1];
+                    pairs[0] = new Pair<View,String>(cygo,"logo");
+
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                    startActivity(intent,options.toBundle());
+                    finish();
+                }
+            },SPLASH_SCREEN);
+        }else{
+            topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+
+            cygo = findViewById(R.id.logo);
+
+            cygo.setAnimation(topAnim);
+
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this,activity_sign_in.class);
+                    Pair[] pairs = new Pair[1];
+                    pairs[0] = new Pair<View,String>(cygo,"logo");
+
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                    startActivity(intent,options.toBundle());
+                    finish();
+                }
+            },SPLASH_SCREEN);
+        }
 
 
 
-        logo.setAnimation(topAnim);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this,Login.class);
-                Pair[] pairs = new Pair[1];
-                pairs[0] = new Pair<View,String>(logo,"logoTransition");
-
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
-                startActivity(intent,options.toBundle());
-                finish();
-            }
-        },SPLASH_SCREEN);
     }
+
+
 }
