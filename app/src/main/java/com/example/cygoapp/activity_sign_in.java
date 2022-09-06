8983d58715256e03e1b481eab3538253b919b964
NDbiz <nadeeshadilshan6@gmail.com>
2022-09-07 01:14:33 +0530

    done splash and get started

--- app/src/main/java/com/example/cygoapp/activity_sign_in.java
+++ app/src/main/java/com/example/cygoapp/activity_sign_in.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUp extends AppCompatActivity {
+public class activity_sign_in extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up2);
+        setContentView(R.layout.activity_sign_in);
     }
 }
